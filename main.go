375f9bab2295086ee22728b8598c277eb4f1d970
sbr <sascha.ramin@sky.uk>
2017-03-09 20:56:55 +0000

    changed to use switches and unhappy case first

--- main.go
+++ main.go
@@ -25,32 +25,27 @@ func main() {
 	baseURL = *u
 	pretty = *p
 
-	if job == "" || baseURL == "" {
+	switch {
+	case (job != "crawl" && job != "review") || baseURL == "":
 		fmt.Println("You must specify the job type and a URL")
+		fmt.Println("eg. clamber -url https://sbramin.com -job crawl")
 		fmt.Println("use clamber -h for more information")
 		os.Exit(1)
-	}
-
-	if job == "review" || job == "crawl" {
+	case job == "crawl" || job == "review":
 		if baseURL[len(baseURL)-1:] != "/" {
 			baseURL += "/"
 		}
-
 		db := boltOn(job)
 		defer boltOff(db)
-
-		if job == "crawl" { // Can change this to a switch
-			start := time.Now()
-			goCrawl(baseURL)
-			fmt.Printf(
-				"Crawled %d pages from %s in %.2f seconds \n", atomic.LoadUint64(&pageCount), baseURL, time.Since(start).Seconds())
-
-		} else if job == "review" {
-			review()
-		}
-	} else {
-		fmt.Println("Job can be crawl or review, not", job)
-		os.Exit(1)
 	}
 
+	switch job {
+	case "crawl":
+		start := time.Now()
+		goCrawl(baseURL)
+		fmt.Printf(
+			"Crawled %d pages from %s in %.2f seconds \n", atomic.LoadUint64(&pageCount), baseURL, time.Since(start).Seconds())
+	case "review":
+		review()
+	}
 }
